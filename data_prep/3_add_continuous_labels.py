75d2b904d034614f268e85df06972ee285cda270
Peter-Devine <peter@lightblue-tech.com>
2024-12-14 11:54:29 +0800

    Update cont label to output probs and make final labels into 7base

--- data_prep/3_add_continuous_labels.py
+++ data_prep/3_add_continuous_labels.py
@@ -5,6 +5,7 @@ from vllm import LLM, SamplingParams
 import os
 from multiprocessing import Pool
 import math
+import numpy as np
 
 def get_hash(example):
     """Get hash of question field."""
@@ -60,31 +61,33 @@ def get_ds(model_name):
 
     return new_ds
 
+get_prob = lambda logprob_dict, tok_id: np.exp(logprob_dict[tok_id].logprob) if tok_id in logprob_dict.keys() else 0
+
 def generate_responses(inputs):
-    text_list, model_name, gpu_id = inputs
+    text_list, model_name, gpu_id, reverse_context_query = inputs
     os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
 
     llm = LLM(model=model_name)
 
-    sampling_params = SamplingParams(temperature=0.0, max_tokens=2)
-    system_message = "You are a relatedness rating assistant. Given a piece of text and a query, output the level that the query relates to the text. Your output should be single number between 1-5, with 1 meaning completely unrelated, 2 meaning mostly unrelated, 3 being unsure as to whether it is related or not, 4 being mostly related, and 5 being completely related."
+    sampling_params = SamplingParams(temperature=0.0, max_tokens=1, logprobs=10)
+    
+    text_query_sys_msg_snippet_1 = "query and a piece of text" if reverse_context_query else "piece of text and a query"
+    text_query_sys_msg_snippet_2 = "text relates to the query" if reverse_context_query else "query relates to the text"
+    system_message = f"You are a relatedness rating assistant. Given a {text_query_sys_msg_snippet_1}, output the level that the {text_query_sys_msg_snippet_2}. Your output should be single number between 1-5, with 1 meaning completely unrelated, 2 meaning mostly unrelated, 3 meaning unsure as to whether it is related or not, 4 meaning mostly related, and 5 meaning completely related."
 
     chats = [[
         {"role": "system", "content": system_message},
-        {"role": "user", "content": f"{c}\n\n<<<Query>>>{q}"},
-    ] for c, q in text_list]
+        {"role": "user", "content": f"<<<Query>>>\n{q}\n\n\n<<<Text>>>\n{t}" if reverse_context_query else f"<<<Text>>>\n{t}\n\n\n<<<Query>>>\n{q}"},
+    ] for t, q in text_list]
 
     responses = llm.chat(chats, sampling_params)
 
-    return [x.outputs[0].text.strip() for x in responses]
+    tok = llm.llm_engine.tokenizer.tokenizer
+    idx_tokens = [tok.encode(str(i))[0] for i in range(1, 6)] # Get token IDs for the tokens "1", "2", "3", "4", and "5"
+    probs = np.array([[get_prob(r.outputs[0].logprobs[0], y) for y in idx_tokens] for r in responses])
+    return probs.tolist()
 
-flatten = lambda xss: [x for xs in xss for x in xs]
-
-if __name__ == '__main__':
-    model_name = "Qwen/Qwen2.5-32B-Instruct-GPTQ-Int4"
-    new_ds = get_ds(model_name)
-    all_texts = list(zip(new_ds["context"], new_ds["question"]))
-
+def get_scores(all_texts, reverse_context_query):
     num_gpus = 8
     batch_size = int(math.ceil(len(all_texts) / num_gpus))
 
@@ -93,7 +96,8 @@ if __name__ == '__main__':
     for i in range(num_gpus):
         start_idx = i*batch_size
         end_idx = start_idx + batch_size
-        split_texts_w_idx.append((all_texts[start_idx:end_idx], model_name, i))
+        text_batch = all_texts[start_idx:end_idx]
+        split_texts_w_idx.append((text_batch, model_name, i, reverse_context_query))
 
     with Pool(num_gpus) as p:
         scores_split = p.map(generate_responses, split_texts_w_idx)
@@ -103,8 +107,21 @@ if __name__ == '__main__':
     for score_split in scores_split:
         scores.extend(score_split)
 
-    new_ds = new_ds.add_column("32B_score", scores)
+    return scores
+
+flatten = lambda xss: [x for xs in xss for x in xs]
+
+if __name__ == '__main__':
+    model_name = "Qwen/Qwen2.5-32B-Instruct-GPTQ-Int4"
+    new_ds = get_ds(model_name)
+    all_texts = list(zip(new_ds["context"], new_ds["question"]))
+
+    scores = get_scores(all_texts, reverse_context_query=False)
+    new_ds = new_ds.add_column("32B_score_probs", scores)
+
+    scores = get_scores(all_texts, reverse_context_query=True)
+    new_ds = new_ds.add_column("32B_score_probs_rev", scores)
 
     new_ds = new_ds.sort("row_id")
-    new_ds.to_parquet("lightblue__rag_datasets_selected_32B4scored.parquet")
-    new_ds.push_to_hub("lightblue/rag_datasets_selected_32B4scored")
+    new_ds.to_parquet("lightblue__rag_datasets_selected_32B4scored_probs.parquet")
+    new_ds.push_to_hub("lightblue/rag_datasets_selected_32B4scored_probs")
--- data_prep/4_make_cont_bin_training_data.py
+++ data_prep/4_make_cont_bin_training_data.py
@@ -1,15 +1,15 @@
 from datasets import load_dataset
 from transformers import AutoTokenizer
 
-score_system_message = "Given a piece of text and a query, output a score of 1-5 based on how related the query is to the text. 1 means least related and 5 is most related."
+score_system_message = "Given a piece of text and a query, output a score of 1-7 based on how related the query is to the text. 1 means least related and 7 is most related."
 binary_system_message = "If the query is answerable based on the given context output True, else output False."
 
 def filter_for_correct_scores_only(x):
     return bool(
         bool(
-            x["label"] and bool(x["32B_score_int"] >= 3)
+            x["label"] and bool(x["32B_score_mean_7base"] >= 3.5)
         ) or bool(
-            bool(not x["label"]) and bool(x["32B_score_int"] <= 3)
+            bool(not x["label"]) and bool(x["32B_score_mean_7base"] < 3.5)
         )
     )
 
@@ -18,7 +18,7 @@ def make_continuous_data(x):
         "conversations": [
             { "from": "system", "value": score_system_message },
             { "from": "human", "value":format_text_query(x["context"], x["question"])},
-            { "from": "gpt", "value": str(int(x["32B_score_int"])) } ]
+            { "from": "gpt", "value": str(int(x["32B_score_mean_7base_round"])) } ]
     }
 
 def make_binary_data(x):
@@ -41,11 +41,25 @@ def parse_int(int_str):
 format_text_query = lambda t, q: f"{t}\n\n<<<Query>>>\n{q}"
 
 def main():
-    ds = load_dataset("lightblue/rag_datasets_selected_32B4scored", split="train")
+    ds = load_dataset("lightblue/rag_datasets_selected_32B4scored_probs", split="train")
 
-    ds = ds.map(lambda x: {"32B_score_int": parse_int(x["32B_score"])}, num_proc=32)
+    ds = ds.map(lambda x: {"32B_score_probs_mean": np.array([
+        x["32B_score_probs"], x["32B_score_probs_rev"]
+        ]).mean(axis=0)}, num_proc=16)
 
-    ds = ds.filter(lambda x: x["32B_score_int"] is not None, num_proc=32)
+    ds = ds.map(lambda x: {
+        "32B_score_mean_exp_val": np.array(
+            [(i+1) * x["32B_score_probs_mean"][i] for i in range(5)]
+        ).sum()},
+        num_proc=16)
+
+    ds = ds.map(lambda x: {
+        "32B_score_mean_7base": x["32B_score_mean_exp_val"] * (7/5)},
+        num_proc=16)
+
+    ds = ds.map(lambda x: {
+        "32B_score_mean_7base_round": round(x["32B_score_mean_7base"])},
+        num_proc=16)
 
     ds = ds.map(make_continuous_data, num_proc=32)
 
@@ -61,38 +75,38 @@ def main():
 
     ds = ds.shuffle()
 
-    num_train_data = int(len(ds) * 0.995)
+    num_train_data = int(len(ds) * 0.95)
 
     tr_ds = ds.select(range(0, num_train_data))
     te_ds = ds.select(range(num_train_data, len(ds)))
 
-    tr_ds.push_to_hub(
-        "lightblue/reranker_continuous_train", private=True
-    )
+    # tr_ds.push_to_hub(
+    #     "lightblue/reranker_continuous_train", private=True
+    # )
 
-    te_ds.push_to_hub(
-        "lightblue/reranker_continuous_val", private=True
-    )
+    # te_ds.push_to_hub(
+    #     "lightblue/reranker_continuous_val", private=True
+    # )
 
     tr_ds.filter(filter_for_correct_scores_only, num_proc=32).push_to_hub(
-        "lightblue/reranker_continuous_filt_train", private=True
+        "lightblue/reranker_continuous_filt_7base_train", private=True
     )
 
     te_ds.filter(filter_for_correct_scores_only, num_proc=32).push_to_hub(
-        "lightblue/reranker_continuous_filt_val", private=True
+        "lightblue/reranker_continuous_filt_7base_val", private=True
     )
 
-    tr_ds.filter(filter_for_correct_scores_only, num_proc=32).map(
-        make_binary_data, num_proc=32
-    ).push_to_hub(
-        "lightblue/reranker_binary_filt_train", private=True
-    )
+    # tr_ds.filter(filter_for_correct_scores_only, num_proc=32).map(
+    #     make_binary_data, num_proc=32
+    # ).push_to_hub(
+    #     "lightblue/reranker_binary_filt_train", private=True
+    # )
 
-    te_ds.filter(filter_for_correct_scores_only, num_proc=32).map(
-        make_binary_data, num_proc=32
-    ).push_to_hub(
-        "lightblue/reranker_binary_filt_val", private=True
-    )
+    # te_ds.filter(filter_for_correct_scores_only, num_proc=32).map(
+    #     make_binary_data, num_proc=32
+    # ).push_to_hub(
+    #     "lightblue/reranker_binary_filt_val", private=True
+    # )
 
 if __name__ == '__main__':
     main()
